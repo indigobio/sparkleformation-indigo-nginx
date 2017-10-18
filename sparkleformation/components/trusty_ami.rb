cd910f3f6fdb77f7a59e79ac4bb6d31f78942855
Greg Swallow <gswallow@gmail.com>
2017-10-18 16:22:19 -0400

    No ruby 2.4.

--- sparkleformation/components/trusty_ami.rb
+++ sparkleformation/components/trusty_ami.rb
@@ -1,8 +1,8 @@
 SparkleFormation.component(:trusty_ami) do
   mappings(:region_to_ami) do
-    set!('us-east-1'.disable_camel!, :ami => 'ami-73489b09')
-    set!('us-east-2'.disable_camel!, :ami => 'ami-0e163a6b')
-    set!('us-west-1'.disable_camel!, :ami => 'ami-c66954a6')
-    set!('us-west-2'.disable_camel!, :ami => 'ami-09589b71')
+    set!('us-east-1'.disable_camel!, :ami => 'ami-6967b413')
+    set!('us-east-2'.disable_camel!, :ami => 'ami-d0163ab5')
+    set!('us-west-1'.disable_camel!, :ami => 'ami-466a5726')
+    set!('us-west-2'.disable_camel!, :ami => 'ami-ef61a297')
   end
 end
